b4071a82f2d9dab9590b0b38a07b91b3637772de
bdilpreet66 <bdilpreet66@gmail.com>
2019-04-02 03:57:15 +0530

    Trying something new

--- question_no_17.cpp
+++ question_no_17.cpp
@@ -1,111 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static int cur_pos=0;
+
 struct process_structure{
 	int process_ID;
 	int arrival_time;
 	int burst_time;
 	int priority;
 	int queue_ID;
-}p[50];
-
-struct Queue
-{
-        int capacity=50;
-        int size=0;
-        int front=0;
-        int rear=-1;
-        int process_id[50];
-}q1 ,q2;
-
-int Dequeue(struct Queue Q)
-{
-    /* Removing an element is equivalent to incrementing index of front by one */
-        
-    Q.size--;
-    Q.front++;
-    /* As we fill elements in circular fashion */
-    if(Q.front==Q.capacity)
-    {
-        Q.front=0;
-    }
-    return Q.process_id[Q.front-1];
-}
-
-int front(struct Queue Q)
-{
-    /* Return the element which is at the front*/
-    return (Q.process_id[Q.front]);
-}
-
-void Enqueue(struct Queue Q,int element)
-{
-    /* If the Queue is full, we cannot push an element into it as there is no space for it.*/
-    if(Q.size == Q.capacity)
-    {
-        printf("Queue is Full\n");
-    }
-    else
-    {
-        Q.size++;
-        Q.rear = Q.rear + 1;
-        /* As we fill the queue in circular fashion */
-        if(Q.rear == Q.capacity)
-        {
-            Q.rear = 0;
-        }
-        /* Insert the element in its rear side */
-        Q.process_id[Q.rear] = element;
-        
-    }
-}
+	int Num;
+	int Finish_time;
+	int Remaining_time;
+	int Waiting_time;
+	int Start_time;
+	int Res_time;
+}p1[50],p2[50];
 
 //sort according to arival time
 void sort_arival(int n){
 	struct process_structure a;
 	int min,pos;
 	
-	for(int i=0;i<n;i++){
-		min = p[i].arrival_time;
+	for(int i=0;i<n-1;i++){
+		min = p1[i].arrival_time;
 		pos = i;	
-		for(int j=0;j<n;j++){
-			if(min>p[j].arrival_time){
-				min = p[j].arrival_time;
+		for(int j=i+1;j<n;j++){
+			if(min>p1[j].arrival_time){
+				min = p1[j].arrival_time;
 				pos = j;
 			}
 		}
 		
 		if(i!=pos){
-			a=p[i];
-			p[i]=p[pos];
-			p[pos]=a;
+			a=p1[i];
+			p1[i]=p1[pos];
+			p1[pos]=a;
 		}
 	}
 }
 
 
 //this part containts scheduler config
-void my_scheduler(int n){
-	int index[n],i=0;
-	bool flag = true;
-	if(q1.front<=q1.rear){
-		//code for queue 1 goes here
-		while(flag){
-			
+void priority_sort(int start,int end){
+	struct process_structure a;
+	int max,pos;
+	
+	for(int i=start;i<=end-1;i++){
+		max = p1[i].priority;
+		pos = i;
+		for(int j=i+1;j<=end;j++){
+			if(max>p1[j].priority){
+				max = p1[j].priority;
+				pos = j;
+			}
 		}
-	}
-	else{
-		//code for queue 2 goes here
-		if(p[front(q2)].burst_time>=2){
-			p[front(q2)].burst_time=p[front(q2)].burst_time-2;	
-		}
-		else{
-			p[front(q2)].burst_time=p[front(q2)].burst_time-1;
-		}
-		index[0] = Dequeue(q2);
-		if(p[front(q2)].burst_time!=0){
-			Enqueue(q2,index);
-			p[index[0]].queue_ID=2;
+		
+		if(i!=pos){
+			a=p1[i];
+			p1[i]=p1[pos];
+			p1[pos]=a;
 		}
 	}
 }
@@ -119,27 +73,51 @@ main(){
 	
 	//get the process imformation
 	for(int i=0;i<n;i++){
-		printf("\nEnter the process Id for the process %d :",i+1);
+		printf("\nEnter the process information for the process %d :",i+1);
+		printf("\nprocess Id : ");
 		scanf("%d",&val);
-		p[i].process_ID = val;
-		printf("\nEnter the process arival time :");
+		p1[i].process_ID = val;
+		printf("\nprocess arival time :");
 		scanf("%d",&val);
-		p[i].arrival_time=val;
-		printf("\nEnter the process burst time :");
+		p1[i].arrival_time=val;
+		printf("\nprocess burst time :");
 		scanf("%d",&val);
-		p[i].burst_time=val;
-		printf("\nEnter the process priority :");
+		p1[i].burst_time=val;
+		printf("\nprocess priority :");
 		scanf("%d",&val);
-		p[i].priority=val;
+		p1[i].priority=val;
+		p1[i].Remaining_time=p[i].burst_time;
+		p1[i].Num = i+1;
 	}
 
 	//sort the processes
+	int start_pos,end_pos,z;
+	bool flag;
+	z=0;
 	sort_arival(n);
-	
-	//add to the queue 1
-	for(int i=0;i<n;i++){
-		Enqueue(q1,i);
-		p[i].queue_ID=1;
+	start_pos=0;
+	end_pos=0;
+	while(z<n){
+		start_pos=end_pos;
+		flag=true;
+		while(flag){
+			if(p1[z].arrival_time==p1[z+1].arrival_time){
+				end_pos++;
+			}
+			else{
+				end_pos++;
+				flag = false;
+			}
+			z++;
+		}
+		priority_sort(start_pos,end_pos-1);
 	}
+	
+	while(true){
+		if(q1[0]!=NULL){
+		
+		}
+	}
+	
 		
 }
